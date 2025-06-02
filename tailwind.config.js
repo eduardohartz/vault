d2b8da255db1f4be9e83bb9eca1f293e946717f8
Eduardo Hartz <80062347+eduardohartz@users.noreply.github.com>
2025-06-02 14:14:41 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,53 +54,5 @@ module.exports = {
       },
     },
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    nextui({
-      themes: {
-        dark: {
-          colors: {
-            background: "#0D1117",
-            foreground: "#ECEDEE",
-            primary: {
-              50: "#3B096C",
-              100: "#520F83",
-              200: "#7318A2",
-              300: "#9823C2",
-              400: "#c031e2",
-              500: "#DD62ED",
-              600: "#F182F6",
-              700: "#FCADF9",
-              800: "#FDD5F9",
-              900: "#FEECFE",
-              DEFAULT: "#DD62ED",
-              foreground: "#ffffff",
-            },
-            focus: "#F182F6",
-          },
-        },
-        light: {
-          colors: {
-            background: "#FFFFFF",
-            foreground: "#11181C",
-            primary: {
-              50: "#FEECFE",
-              100: "#FDD5F9",
-              200: "#FCADF9",
-              300: "#F182F6",
-              400: "#DD62ED",
-              500: "#c031e2",
-              600: "#9823C2",
-              700: "#7318A2",
-              800: "#520F83",
-              900: "#3B096C",
-              DEFAULT: "#c031e2",
-              foreground: "#ffffff",
-            },
-            focus: "#c031e2",
-          },
-        },
-      },
-    }),
-  ],
+  plugins: [require("tailwindcss-animate")],
 }
