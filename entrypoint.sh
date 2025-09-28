0b05e341b8240d764f2b40307df82b40f7797296
Eduardo Hartz <80062347+eduardohartz@users.noreply.github.com>
2025-09-28 07:00:03 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -e
 
-TABLE_COUNT=$(PGPASSWORD=$POSTGRES_PASSWORD psql -h postgresql -U $POSTGRES_USER -d $POSTGRES_DB -t -c \
+export PGPASSWORD=$POSTGRES_PASSWORD
+
+TABLE_COUNT=$(psql -h postgresql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -t -c \
   "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema='public';" | xargs)
+
+TABLE_COUNT=${TABLE_COUNT:-1}
 
 if [ "$TABLE_COUNT" -eq 0 ]; then
   echo "Database empty, running prisma db push..."
